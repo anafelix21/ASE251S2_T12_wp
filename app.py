367e355a183338cfc1c39f7512602f111ac09d7d
Ana Félix <ana.felix@vallegrande.edu.pe>
2025-10-02 02:09:08 -0500

    Primer commit - subiendo proyecto

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/pago')
+def pago():
+    return render_template('pago.html')
+
+@app.route('/categorias')
+def categorias():
+    return render_template('categorias.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
